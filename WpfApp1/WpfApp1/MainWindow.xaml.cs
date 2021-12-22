3aada0d7db9407a3adc5527a843f6cb6b5f72011
jules08217 <yulya-savina1@bk.ru>
2021-12-22 19:32:56 +0500

    commit

---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Update();
+        }
+
+        public void Update()
+        {
+            var list = App.DB.agents.ToList();
+            listView.ItemsSource = list;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            agents agent = new agents
+            {
+                FirstName = fNameTextBox.Text,
+                MiddleName = mNameTextBox.Text,
+                LastName = lNameTextBox.Text,
+                DealShare = Convert.ToInt32(dShareTextBox.Text)
+            };
+            App.DB.agents.Add(agent);
+            App.DB.SaveChanges();
+            MessageBox.Show("Данные добавлены");
+            Update();
+
+        }
+    }
+}
