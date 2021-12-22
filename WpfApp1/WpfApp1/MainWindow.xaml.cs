210b758cabc8484d266e4d49b13c2c0d54ea13fc
jules08217 <yulya-savina1@bk.ru>
2021-12-22 20:11:48 +0500

    коммит3

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -47,5 +47,40 @@ namespace WpfApp1
             Update();
 
         }
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            var buttonDelete = (Button)sender;
+            var agent = (agents)buttonDelete.DataContext;
+
+            App.DB.agents.Remove(agent);
+            App.DB.SaveChanges();
+            MessageBox.Show($"{agent.FirstName} {agent.MiddleName} удален!");
+            Update();
+        }
+
+        private void ChangeButton_Click(object sender, RoutedEventArgs e)
+        {
+            var buttonDelete = (Button)sender;
+            var agent = (agents)buttonDelete.DataContext;
+            fNameTextBox.Text = agent.FirstName;
+            mNameTextBox.Text = agent.MiddleName;
+            lNameTextBox.Text = agent.LastName;
+            dShareTextBox.Text = Convert.ToString(agent.DealShare);
+            App.change_agent = agent;
+        }
+
+        private void SaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            var ch_agent = App.DB.agents.Where(p => p.Id == App.change_agent.Id).FirstOrDefault();
+            ch_agent.FirstName = fNameTextBox.Text;
+            ch_agent.MiddleName = mNameTextBox.Text;
+            ch_agent.LastName = lNameTextBox.Text;
+            ch_agent.DealShare = Convert.ToInt32(dShareTextBox.Text);
+
+            App.DB.SaveChanges();
+            MessageBox.Show("Данные изменены");
+            Update();
+        }
     }
 }
